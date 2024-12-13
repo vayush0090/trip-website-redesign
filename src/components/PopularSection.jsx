efeccba3fcc38e5dd3677bf2c584fc894b6be340
Ayush <ayushverma1016@gmail.com>
2024-12-13 18:36:55 +0530

    updated images

--- src/components/PopularSection.jsx
+++ src/components/PopularSection.jsx
@@ -21,42 +21,42 @@ const PopularSection = () => {
             <PopularSectionCard/> */}
         <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/12446112/pexels-photo-12446112.jpeg?auto=compress&cs=tinysrgb&w=600"
+            src="https://images.unsplash.com/photo-1732639840730-a3cdf066a9e6?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
         </div>
-        <div className="bg-red-500 rounded-3xl w-[70vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
+        <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/28749616/pexels-photo-28749616/free-photo-of-taj-mahal-in-agra-on-a-sunny-day.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src="https://images.unsplash.com/photo-1645980901327-14dcec2543f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
         </div>
-        <div className="bg-red-500 rounded-3xl w-[70vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
+        <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/1174732/pexels-photo-1174732.jpeg?auto=compress&cs=tinysrgb&w=600"
+            src="https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
         </div>
-        <div className="bg-red-500 rounded-3xl w-[70vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
+        <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/16952108/pexels-photo-16952108/free-photo-of-india-gate-silhouette-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=600"
+            src="https://plus.unsplash.com/premium_photo-1682091942392-bc2842010e75?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
         </div>
-        <div className="bg-red-500 rounded-3xl w-[70vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
+        <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/2063892/pexels-photo-2063892.jpeg?auto=compress&cs=tinysrgb&w=600"
+            src="https://images.unsplash.com/photo-1456334694595-f251aa3e1cd0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
         </div>
-        <div className="bg-red-500 rounded-3xl w-[70vw] h-[50vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
+        <div className="bg-red-500 rounded-3xl w-[70vw] h-[40vh] sm:w-[50vw] sm:h-[50vh] md:w-[35vw] md:h-[50vh] overflow-hidden shadow-2xl">
           <img
-            src="https://images.pexels.com/photos/13238814/pexels-photo-13238814.jpeg?auto=compress&cs=tinysrgb&w=600"
+            src="https://plus.unsplash.com/premium_photo-1673697239984-b089baf7b6e6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             className="w-full hover:scale-150 object-cover h-[100%]   transition-all ease-in duration-500 cursor-grab"
             alt=""
           />
