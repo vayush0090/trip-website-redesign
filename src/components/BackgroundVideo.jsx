860f1d3badb8d9283ccce354cefd09e34bcdf86a
Ayush <ayushverma1016@gmail.com>
2024-12-13 18:19:44 +0530

    change the path

--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -6,7 +6,7 @@ const BackgroundVideo = () => {
         {/* w-[80vw] h-[70vh] */}
       <video autoPlay loop muted playsInline className="background-video mx-auto rounded-3xl w-[80vw] h-[70vh] object-cover object-center">
       {/* h-[80vh] w-[150vw]  */}
-        <source src="/src/video/video.mp4" type="video/mp4" />
+        <source src="/video/video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       
